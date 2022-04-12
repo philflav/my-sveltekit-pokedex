0c89423efef8c75dd51f9326bd7423e7c9044e96
Phil <pflavin@gmail.com>
2022-04-12 16:30:58 +0100

    Inital Pokeman card added

---
+++ src/components/pokemanCard.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let pokeman
+</script>
+<a class="p-6 bg-gray-200 text-gray-900 text-center, rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
+href={'/pokeman/${pokeman.id}'}>
+    <img class="h-40 w-40" src={pokeman.image} alt="{pokeman.name}">
+    <h2 class="uppercase text-2xl">{pokeman.id}. {pokeman.name}</h2>
+
+</a>
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,18 @@
 <script>
     import {pokemon} from "../stores/pokestore"
+    import PokemanCard from "../components/pokemanCard.svelte"
 </script>
 
 
 <svelte:head><title>Sveltkit Pokedex</title></svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
-
-{#each  $pokemon as pokeman}
-<p>{pokeman.name}</p>    
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+    {#each  $pokemon as pokeman}
+    <PokemanCard pokeman={pokeman}/> 
 {/each}
+</div>
+
 
 <style>
     h1{
