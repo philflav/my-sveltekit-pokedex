32b7126ff234bfb880f19b5661c6e2a6acda0a36
Phil <pflavin@gmail.com>
2022-04-12 10:22:28 +0100

    Home and about with Nav

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to My SvelteKit-Pokedex</h1>
+<script>
+
+
+</script>
+
+<h1>SvelteKit Pokedex</h1>
+
+<svelte:head><title>Sveltkit Pokedex</title></svelte:head>
+
+<style>
+    h1{
+        color:rgb(137, 98, 174)
+        }    
+</style>
 
