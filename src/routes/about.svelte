358783339d670ac2af465c2a1737549f70a95d52
Phil <pflavin@gmail.com>
2022-04-14 19:30:19 +0100

    About page updated

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,7 @@
+<div>
 <h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<div class="text-center ">
+    <p class="text-blue-900">This is an example of a website built using Sveltekit and hosted on Cloudflare pages. </p>
+    <p >The app follows <a class = "text-blue-900" href='https://youtu.be/UU7MgYIbtAk'>this Sveltkit Crash course.</a></p>
+</div>
+</div>
