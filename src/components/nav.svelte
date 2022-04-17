146ddb1bda8c2ec88479d53beb0d09f2fe4f1c9a
Phil <pflavin@gmail.com>
2022-04-17 10:46:17 +0100

    FIrebase branch

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,16 +1,9 @@
 <nav
-	class="relative w-full flex flex-wrap items-center justify-between py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light"
+	class="relative w-full flex flex-wrap items-center justify-around py-3 bg-gray-900 text-gray-200 shadow-lg navbar navbar-expand-lg navbar-light"
 >
 	<a class="nav-link text-white opacity-80 hover:opacity-100 focus:opacity-100 p-0" href="/">Home</a
 	>
 	<a class="nav-link text-white opacity-80 hover:opacity-100 focus:opacity-100 p-0" href="/about"
 		>About</a
-	>
-	<a
-		class="nav-link text-white opacity-80 hover:opacity-100 focus:opacity-100 p-0"
-		href="/ninja-gaming">Ninja Gaming</a
-	>
-	<a class="nav-link text-white opacity-80 hover:opacity-100 focus:opacity-100 p-0" href="/docs"
-		>Documents</a
 	>
 </nav>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -3,14 +3,8 @@
 	<div class="text-center ">
 		<p>This is an example of a website built using Sveltekit and hosted on Cloudflare pages.</p>
 		<p>
-			The app follows <a class="text-blue-900" href="https://youtu.be/UU7MgYIbtAk"
-				>this Sveltkit Crash course.</a
-			>
-		</p>
-		<p>
-			Added a Ninja gaming page to follow <a
-				href="https://youtu.be/WK4SN853CqI?list=PL4cUxeGkcC9hpM9ARM59Ve3jqcb54dqiP"
-				>Net Ninja Svelte Crash Course</a
+			This app demonstrates Firebase authentication following <a href="https://youtu.be/XIiOhorRwUg"
+				>this YouTube tutorial</a
 			>
 		</p>
 	</div>
