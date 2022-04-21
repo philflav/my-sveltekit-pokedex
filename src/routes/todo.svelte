cf473fbe3c3bc8bf976d6713db74e95626ed8026
Phil <pflavin@gmail.com>
2022-04-21 16:35:05 +0100

    Minor formatting changes

--- src/routes/todo.svelte
+++ src/routes/todo.svelte
@@ -67,55 +67,64 @@
 </script>
 
 <h1 class="text-4xl text-center my-8 uppercase">{displayName}'s Todo List</h1>
+<div class="container mx-auto">
+	{#if $isLoggedIn}
+		<input class="text-lg6 text-center mx-auto" type="text" {placeholder} bind:value={newItem} />
+		<div class="flex flex-col">
+			<button
+				class="bg-green-500 hover:bg-green-700 text-base text-white px-4 rounded-full m-3"
+				on:click={() => {
+					addItem(0);
+				}}
+			>
+				Do it Next</button
+			>
+			<button
+				class="bg-orange-500 hover:bg-orange-700 text-base text-white px-4 rounded-full m-3"
+				on:click={() => {
+					addItem(1);
+				}}
+			>
+				Do it Later</button
+			>
+			<button
+				class="bg-blue-400 hover:bg-blue-700 text-base text-white px-4 rounded-full m-3"
+				on:click={() => {
+					addItem(2);
+				}}>Do it Sometime</button
+			>
+		</div>
 
-{#if $isLoggedIn}
-	<input class="text-base" type="text" {placeholder} bind:value={newItem} />
-	<button
-		class="bg-green-500 hover:bg-green-700 text-base text-white px-4 rounded-full"
-		on:click={() => {
-			addItem(0);
-		}}>Add as High Priority</button
-	>
-	<button
-		class="bg-orange-500 hover:bg-orange-700 text-base text-white px-4 rounded-full"
-		on:click={() => {
-			addItem(1);
-		}}>Add as Normal Priority</button
-	>
-	<button
-		class="bg-blue-400 hover:bg-blue-700 text-base text-white px-4 rounded-full"
-		on:click={() => {
-			addItem(2);
-		}}>Add as Low Priority</button
-	>
-
-	<div class="border-2 m-10  p-10">
-		<ol>
-			{#each todos as td}
-				<li class="list-decimal text-left text-base" class:complete={td.isComplete}>
-					<span
-						><button
-							class="bg-blue-500 hover:bg-blue-700 text-white m-2 px-2 rounded-full"
-							on:click={() => markCompleted(td)}>✔</button
-						></span
-					>
-					<span class={td.bg}>{td.task}</span>
-					<span
-						><button
-							class="bg-red-500 hover:bg-red-700 text-white m-2 px-2 rounded-full"
-							on:click={() => deleteTodo(td)}>✘</button
-						></span
-					>
-				</li>
-			{:else}
-				<p>No todos found</p>
-			{/each}
-		</ol>
-	</div>
-{/if}
-{#if !$isLoggedIn}
-	<div class="text-center"><h2>User not logged in</h2></div>
-{/if}
+		<div class="border-2 m-10  p-5">
+			<ol>
+				{#each todos as td}
+					<li class="list-decimal text-left text-base" class:complete={td.isComplete}>
+						<span
+							><button
+								title="Mark task as completed"
+								class="bg-blue-500 hover:bg-blue-700 text-white m-2 px-1 rounded-full"
+								on:click={() => markCompleted(td)}>✔</button
+							></span
+						>
+						<span class={td.bg}>{td.task}</span>
+						<span
+							><button
+								title="Delete task"
+								class="bg-red-500 hover:bg-red-700 text-white m-2 px-1 rounded-full"
+								on:click={() => deleteTodo(td)}>✘</button
+							></span
+						>
+					</li>
+				{:else}
+					<p>No todos found</p>
+				{/each}
+			</ol>
+		</div>
+	{/if}
+	{#if !$isLoggedIn}
+		<div class="text-center"><h2>User not logged in</h2></div>
+	{/if}
+</div>
 
 <svelte:window on:keypress={keyPressed} />
 
